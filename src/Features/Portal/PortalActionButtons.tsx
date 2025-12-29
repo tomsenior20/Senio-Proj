da4c8b79bdcb363f3ea44478b4eed4f185870d18
tomsenior20 <tomsenior20@gmail.com>
2025-12-29 18:52:01 +0000

    added menu button to collpase side bar

--- src/Features/Portal/PortalActionButtons.tsx
+++ src/Features/Portal/PortalActionButtons.tsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { FaCog } from 'react-icons/fa';
+import '../../styling/sidebar.scss';
+
+interface AdminActionProp {
+  isAdmin: boolean;
+  showAdminContainer: boolean;
+}
 
 function UserSearch_Click() {
   alert('Users search');
@@ -9,29 +16,47 @@ function AccessManagement_Click() {
   alert('Access Management Clicked');
 }
 
-export default function AdminAction() {
+export default function AdminAction({
+  isAdmin,
+  showAdminContainer,
+}: AdminActionProp) {
+  const [AdminMenu, setAdminMenu] = useState<boolean>();
+
+  useEffect(() => {
+    setAdminMenu(isAdmin);
+  }, []);
+
   return (
-    <div className='adminAccessContainer'>
-      <ul>
-        <li>
-          <button
-            className='btn btn-active btn-primary w-full'
-            onClick={UserSearch_Click}
-          >
-            <FaSearch className='adminIcon' />
-            <span className='buttonText'>User Search</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className='btn btn-active btn-primary w-full'
-            onClick={AccessManagement_Click}
-          >
-            <FaCog className='adminIcon' />
-            <span className='buttonText'>Access Management</span>
-          </button>
-        </li>
-      </ul>
-    </div>
+    <>
+      {AdminMenu && (
+        <div
+          className={`adminAccessContainer ${
+            showAdminContainer ? '' : 'closed'
+          }`}
+        >
+          <ul>
+            <li>
+              <button
+                className='btn btn-active btn-primary w-full'
+                onClick={UserSearch_Click}
+              >
+                <FaSearch className='adminIcon' />
+                <span className='buttonText'>User Search</span>
+              </button>
+            </li>
+            <li>
+              <button
+                className='btn btn-active btn-primary w-full'
+                onClick={AccessManagement_Click}
+              >
+                <FaCog className='adminIcon' />
+                <span className='buttonText'>Access Management</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+      ;
+    </>
   );
 }
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,18 +1,53 @@
 import '../../styling/sidebar.scss';
 import AdminAction from '../../features/Portal/PortalActionButtons';
+import { useState } from 'react';
 
 interface UserLevel {
   levelOfUser: boolean;
 }
 
 export default function Sidebar({ levelOfUser }: UserLevel) {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+  // Function for menu click
+  function MenuClick() {
+    setSidebarOpen(!sidebarOpen);
+  }
+
   return (
-    <div className='sideBarContainer'>
-      <div className='levelContainer'>
+    <div
+      className={`sideBarContainer ${sidebarOpen ? '' : 'closed'}`}
+      id='sideBarContainer'
+      aria-label='sidebar'
+    >
+      <div
+        className={`p-2! m-1! flex ${
+          sidebarOpen ? 'justify-end' : 'justify-center'
+        }`}
+      >
+        <button
+          className='btn btn-sm btn-square bg-transparent'
+          onClick={MenuClick}
+          aria-label='Mobile SideBar Button'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            x='0px'
+            y='0px'
+            width='50'
+            height='50'
+            viewBox='0 0 30 30'
+          >
+            <path d='M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z'></path>
+          </svg>
+        </button>
+      </div>
+      <div className={`levelContainer  ${sidebarOpen ? '' : 'closed'}`}>
         <h1 className='leveluserText'>Level of user: </h1>
         <h2 className='levelText'>{levelOfUser ? 'Admin' : 'Non Admin'}</h2>
       </div>
-      {levelOfUser && <AdminAction />}
+      {levelOfUser && (
+        <AdminAction isAdmin={levelOfUser} showAdminContainer={sidebarOpen} />
+      )}
     </div>
   );
 }
