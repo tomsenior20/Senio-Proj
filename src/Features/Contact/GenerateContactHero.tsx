721e8c4422d8ccad2bd64c475c72b52f421e12af
tomsenior20 <tomsenior20@gmail.com>
2026-01-12 15:52:09 +0000

    r-factored contact hero section

--- src/Features/Contact/GenerateContactHero.tsx
+++ src/Features/Contact/GenerateContactHero.tsx
@@ -6,13 +6,55 @@ interface ContactLinksProps {
   name: string;
   value: string;
 }
-export default function GenerateContactHero() {
-  const [contactLinks, setContactLinks] = useState<ContactLinksProps[]>([]);
 
+interface GenerateContactLinksProps {
+  contactLinks: ContactLinksProps[];
+}
+
+function ContactHeroHeader() {
+  return (
+    <div className='helpContentContainer'>
+      <h1 className='helpHeader'>Need Help?</h1>
+      <p className='helpText'>
+        We're here to help! Fill out the form beside to reach us. Our customer
+        care team will be available to answer any futher queries and will
+        alternatively respond to the contact query. Please allow 24-48 hours
+        during operational times for the product.
+      </p>
+    </div>
+  );
+}
+
+function GenerateContactLink({ contactLinks }: GenerateContactLinksProps) {
   // Used to Add Space at the end of contact methods
   function replaceContactAlias(name: string) {
     return name.replace('Contact', '');
   }
+
+  return (
+    <div className='contactLinkContainer'>
+      <table className='table w-full tablecontainer p-2'>
+        <tbody>
+          {contactLinks.map((contactLink) => (
+            <tr className='contactLinksList' key={contactLink.name}>
+              <td className='flex w-full'>
+                <p className='contactLinksMethod' aria-label='contactLink'>
+                  {replaceContactAlias(contactLink.name)}
+                </p>
+                <p className='contactLinks' aria-label='contact value'>
+                  {contactLink.value}
+                </p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default function GenerateContactHero() {
+  const [contactLinks, setContactLinks] = useState<ContactLinksProps[]>([]);
 
   // Async function to get contact information from db
   useEffect(() => {
@@ -22,10 +64,6 @@ export default function GenerateContactHero() {
         parameter: 'name',
         value: 'Contact',
       });
-
-      console.log('APIGet returned:', data);
-      console.log('Is array?', Array.isArray(data));
-
       setContactLinks(data);
     }
 
@@ -34,33 +72,8 @@ export default function GenerateContactHero() {
 
   return (
     <div className='helpContainer'>
-      <div className='helpContentContainer'>
-        <h1 className='helpHeader'>Need Help?</h1>
-        <p className='helpText'>
-          We're here to help! Fill out the form beside to reach us. Our customer
-          care team will be available to answer any futher queries and will
-          alternatively respond to the contact query. Please allow 24-48 hours
-          during operational times for the product.
-        </p>
-      </div>
-      <div className='contactLinkContainer'>
-        <table className='table w-full tablecontainer p-2'>
-          <tbody>
-            {contactLinks.map((contactLink) => (
-              <tr className='contactLinksList' key={contactLink.name}>
-                <td className='flex w-full'>
-                  <p className='contactLinksMethod' aria-label='contactLink'>
-                    {replaceContactAlias(contactLink.name)}
-                  </p>
-                  <p className='contactLinks' aria-label='contact value'>
-                    {contactLink.value}
-                  </p>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <ContactHeroHeader />
+      <GenerateContactLink contactLinks={contactLinks} />
     </div>
   );
 }
